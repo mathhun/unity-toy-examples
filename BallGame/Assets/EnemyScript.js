7b7c1cf93c0e59555718c19f5b863ddbbbf6bc03
mathhun <achemer@gmail.com>
2015-05-04 14:43:01 +0900

    Unity Sample Projects from dotinstall

---
+++ BallGame/Assets/EnemyScript.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Update () {
+    transform.position.z -= 0.1;
+    var n : int = 8;
+    transform.Rotate(Random.value*n, Random.value*n, Random.value*n);
+
+    if (transform.position.z < -12.0) {
+        Application.LoadLevel("GameOver");
+    }
+}
+
+function OnCollisionEnter() {
+    Destroy(gameObject);
+}
