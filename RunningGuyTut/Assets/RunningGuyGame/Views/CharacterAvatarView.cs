cf23830a05f61b30563dc2a7edd8a0ab02ecab03
mathhun <achemer@gmail.com>
2015-05-08 18:19:39 +0900

    Player can jump

--- RunningGuyTut/Assets/RunningGuyGame/Views/CharacterAvatarView.cs
+++ RunningGuyTut/Assets/RunningGuyGame/Views/CharacterAvatarView.cs
@@ -6,7 +6,34 @@ using UnityEngine;
 using UniRx;
 
 
-public partial class CharacterAvatarView {
+public partial class CharacterAvatarView { 
+
+    /// Subscribes to the state machine property and executes a method for each state.
+    public override void JumpStateChanged(Invert.StateMachine.State value) {
+        base.JumpStateChanged(value);
+    }
+    
+    public override void OnNoJump() {
+        base.OnNoJump();
+    }
+    
+    public override void OnDoJump() {
+        base.OnDoJump();
+        var rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, 0);
+        rigidbody2d.AddForce(transform.up * 8, ForceMode2D.Impulse);
+    }
+    
+    public override void OnInTheAir() {
+        base.OnInTheAir();
+    }
+
+    /// Subscribes to the property and is notified anytime the value changes.
+    public override void IsNotOnTheGroundChanged(Boolean value) {
+        base.IsNotOnTheGroundChanged(value);
+        animator.SetBool("IsInTheAir", value);
+    }
+
     Vector3 speed = new Vector3(3, 0, 0);
     protected Animator animator;
     protected Transform art;
@@ -51,5 +78,19 @@ public partial class CharacterAvatarView {
         if (Input.GetKey(KeyCode.A)) return MovementIntention.Left;
         if (Input.GetKey(KeyCode.D)) return MovementIntention.Right;
         return MovementIntention.Stop;
+    }
+
+    protected override JumpIntention CalculateJumpIntention()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            return JumpIntention.Jump;
+        }
+
+        return JumpIntention.Idle;
+    }
+
+    protected override bool CalculateIsOnTheGround()
+    {
+        return Physics2D.Raycast(transform.position, -transform.up.normalized, 0.05f, LayerMask.GetMask(new []{"Floor"}));
     }
 }
