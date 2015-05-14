ea4219e34e2ac94bd587f970ad492bc6b0e5ff0f
mathhun <achemer@gmail.com>
2015-05-14 20:18:40 +0900

    Goal script

--- PillBugProto01/Assets/Scripts/Bug.cs
+++ PillBugProto01/Assets/Scripts/Bug.cs
@@ -1,9 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UniRx;
 
 public class Bug : MonoBehaviour
 {
-	void Update () {
-        transform.position += new Vector3(0.02f, 0.0f, 0.0f);
+    public float delaySec = 0.5f;
+    public Vector3 movement = new Vector3(0.02f, 0.0f);
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Goal") {
+            movement = new Vector3(0f, 0f, 0f);
+
+            Observable.Timer(TimeSpan.FromSeconds(delaySec))
+                .Subscribe(_ => Destroy(this.gameObject));
+        }
+    }
+
+	void Update ()
+    {
+        transform.position += movement;
 	}
 }
---
+++ PillBugProto01/Assets/Scripts/Goal.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //Debug.Log(other.gameObject);
+    }
+}
