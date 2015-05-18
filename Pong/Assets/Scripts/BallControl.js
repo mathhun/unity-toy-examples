9794462099ee53b74aa1f238d6287ca6ce563784
mathhun <achemer@gmail.com>
2015-05-19 08:02:25 +0900

    debug

--- Pong/Assets/Scripts/BallControl.js
+++ Pong/Assets/Scripts/BallControl.js
@@ -4,9 +4,9 @@ function Start () {
     var randomNumber = Random.Range(0.0, 1.0);
     Debug.Log(randomNumber);
     if (randomNumber <= 0.5) {
-        GetComponent.<Rigidbody2D>().AddForce(new Vector2(80, 10));
+        GetComponent.<Rigidbody2D>().AddForce(new Vector2(8, 1));
     } else {
-        GetComponent.<Rigidbody2D>().AddForce(new Vector2(-80, 10));
+        GetComponent.<Rigidbody2D>().AddForce(new Vector2(-8, 1));
     }
 }
 
@@ -14,6 +14,5 @@ function OnCollisionEnter2D(colInfo : Collision2D) {
     if (colInfo.collider.tag == "Player") {
         var velY = GetComponent.<Rigidbody2D>().velocity.y;
         GetComponent.<Rigidbody2D>().velocity.y = GetComponent.<Rigidbody2D>().velocity.y / 2 + colInfo.collider.GetComponent.<Rigidbody2D>().velocity.y / 3;
-    } else {
     }
 }
---
+++ Pong/Assets/Scripts/GameManager.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+static var playerScore01 : int = 0;
+static var playerScore02 : int = 0;
+
+var theSkin : GUISkin;
+
+static function Score(wallName : String) {
+    if (wallName == "RightWall") {
+        playerScore01 += 1;
+    } else {
+        playerScore02 += 1;
+    }
+    Debug.Log(wallName);
+    Debug.Log(playerScore01);
+    Debug.Log(playerScore02);
+    Debug.Break();
+}
+
+function OnGUI() {
+    //GUI.skin = theSkin;
+    //GUI.Label(new Rect(Screen.width/2-150-12, 25, 100, 100), "" + playerScore01);
+    //GUI.Label(new Rect(Screen.width/2+150-12, 25, 100, 100), "" + playerScore02);
+}
